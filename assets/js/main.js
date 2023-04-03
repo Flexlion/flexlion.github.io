b18007629f2220fb578d397eb2154a1acee582f9
TudouHacks <haxxieepic@gmail.com>
2023-04-03 23:10:34 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,19 @@
 const loadAnims = url => {
   dropdown = document.getElementById("dropdown");
-  jQuery.get({
-      url: url,
-      success: data => {
-          data.forEach((weapon, i) => {
-            var lines = data.split("\n");
-            // Loop through the lines and add them as options to the select element
-            for (var i = 0; i < lines.length; i++) {
-              var option = document.createElement("option");
-              option.text = lines[i];
-              dropdown.add(option);
-            }
-          })
+  console.log(url);
+  jQuery.get(url, data => {
+        console.log(data);
+        var lines = data.split("\n");
+        // Loop through the lines and add them as options to the select element
+        for (var i = 0; i < lines.length; i++) {
+          var option = document.createElement("option");
+          option.text = lines[i];
+          dropdown.add(option);
+        }
       },
-  })
-}
+  );
+};
 
 $(document).ready(function () {
   loadAnims("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/animations.txt")
-})
+});
