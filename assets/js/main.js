ad57f07154a21dc16b3d1fa175dc7c382e113d7d
nacho95101 <nacho.l.f.05@gmail.com>
2023-04-04 02:27:44 +0200

    working map selector

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,4 +42,26 @@ $(document).ready(function () {
   $('.gallery-image_pants').click(function(event) {
     click_img(event);
   });
+  $('.gallery-image_map').click(function(event) {
+    click_img(event);
+  });
 });
+
+  // Selecciona todas las imágenes de la galería
+  const galleryImages = document.querySelectorAll('.gallery-image_map');
+
+  // Itera sobre cada imagen de la galería y agrega un evento click a cada una
+  galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+      // Actualiza la imagen en el botón del mapa
+      const mapButton = document.querySelector('map img');
+      mapButton.setAttribute('src', image.src);
+
+      // Obtiene el nombre del mapa a partir del atributo alt de la imagen
+      const mapName = image.alt;
+
+      // Actualiza el texto del elemento p correspondiente al nombre del mapa
+      const mapNameElement = document.querySelector('#mapName');
+      mapNameElement.textContent = mapName;
+    });
+  });
