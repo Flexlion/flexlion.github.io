307e6361b4e6ad900588e31ee1bdcda7582d015d
TudouHacks <haxxieepic@gmail.com>
2023-04-10 14:55:25 +0200

    add some stuff and saving for it

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,3 +31,95 @@ function downloadFile(content, fileName, contentType) {
     a.download = fileName;
     a.click();
 }
+
+function loadClickableIdOptions(galleryId, galleryClassName, imgClassName, totalNum, entryPerLine, getImgUrlFunc, getRsdbIdFunc){
+  gallery_root = document.getElementById(galleryId);
+
+  for(var i = 0; i < totalNum / entryPerLine; i++){
+      gallery = document.createElement("div");
+      gallery.setAttribute("class", galleryClassName)
+      for(var idx = i * entryPerLine; idx < Math.min((i + 1) * entryPerLine, totalNum); idx++){
+          option = new Image();
+          option.src = getImgUrlFunc(idx);
+          option.setAttribute("rsdb_id", getRsdbIdFunc(idx));
+          option.setAttribute("class", imgClassName);
+          gallery.appendChild(option);
+      }
+
+      gallery_root.appendChild(gallery);
+  }
+};
+
+function loadSkinTones(){
+  loadClickableIdOptions("image_gallery_skin", "image_gallery_skin", "player_skintone", 9, 9, 
+  idx => {
+      return "./assets/img/player/skin_color/" + String(idx) + ".png";
+  }, 
+  idx => {
+      return idx;
+  });
+};
+
+function loadEyeColors(){
+  loadClickableIdOptions("image_gallery_eyecolor", "image_gallery_eyecolor", "player_eyecolor", 21, 7, 
+  idx => {
+      return "./assets/img/player/eye_color/" + String(idx) + ".png";
+  }, 
+  idx => {
+      return idx;
+  });
+};
+
+function loadHairs(){
+  loadClickableIdOptions("image_gallery_hair", "image_gallery_hair", "player_hair", HairInfo.length, 8, 
+  idx => {
+      return "./assets/img/player/hair/" + HairInfo[idx]["__RowId"] + ".png";
+  }, 
+  idx => {
+      return HairInfo[idx]["Id"];
+  });
+  hairOpts = document.getElementsByClassName("player_hair");
+  for(var i = 0; i < hairOpts.length; i++){
+      if(hairOpts[i].getAttribute("rsdb_id") == "500"){
+          hairOpts[i].src = "./assets/img/player/hair/" + getRsdbInfoById(HairInfo, 500)["__RowId"] + "_F.png";
+          hairOpts[i].setAttribute("id", "hair_msn310");
+      }
+  }
+};
+
+function loadEyebrows(){
+  loadClickableIdOptions("image_gallery_eyebrow", "image_gallery_eyebrow", "player_eyebrow", EyebrowInfo.length, 4, 
+  idx => {
+      return "./assets/img/player/eyebrow/" + EyebrowInfo[idx]["__RowId"] + "_F.png";
+  },
+  idx => {
+      return EyebrowInfo[idx]["Id"];
+  });
+};
+
+function loadPants(){
+  var validInfos = [];
+  for(var i = 0; i < BottomInfo.length; i++){
+      if(BottomInfo[i]["Order"] == -1) continue;
+      validInfos.push(BottomInfo[i]);
+  }
+  loadClickableIdOptions("image_gallery_pants", "image_gallery_pants", "player_bottom", validInfos.length, validInfos.length, 
+  idx => {
+      return "./assets/img/player/pants/" + validInfos[idx]["__RowId"] + ".png";
+  },
+  idx => {
+      return validInfos[idx]["Id"];
+  });
+};
+
+function onChangePlayerType(playerType){
+  playerType = Number(playerType);
+
+  var suffix = "_F.png";
+  if(playerType & 1) suffix = "_M.png";
+
+  eyebrowOpts = document.getElementsByClassName("player_eyebrow");
+  for(var i = 0; i < eyebrowOpts.length; i++) eyebrowOpts[i].src = "./assets/img/player/eyebrow/" + getRsdbInfoById(EyebrowInfo, eyebrowOpts[i].getAttribute("rsdb_id"))["__RowId"] + suffix;
+
+  document.getElementById("hair_msn310").src = "./assets/img/player/hair/" + getRsdbInfoById(HairInfo, 500)["__RowId"] + suffix;
+};
--- assets/js/saveeditor.js
+++ assets/js/saveeditor.js
@@ -5,6 +5,7 @@ var GearInfoShoes;
 var BottomInfo;
 var HairInfo;
 var EyebrowInfo;
+var NameplateInfo;
 var langEUen;
 
 var SaveJson;
@@ -12,11 +13,32 @@ var SaveRaw;
 
 var SaveEdits = {};
 
+function click_clickable_sett(target)
+{
+    var className = target.getAttribute("class");
+    var elements = document.getElementsByClassName(className);
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].setAttribute("selected", "false");
+    }
+    target.setAttribute("selected", "true");
+};
+
 function loadSave(){
+    SaveEdits = {};
     document.getElementById("player_name_holder").value = SaveJson["server"]["UserName"];
 	document.getElementById("player_identifier_holder").value = SaveJson["server"]["Identifier"];
-	document.getElementById("player_rank_holder").value = SaveJson["server"]["PlayerRank"];
+	document.getElementById("player_rank_holder").value = SaveJson["server"]["PlayerRank"] + 1;
 	document.getElementById("player_rank_exp_holder").value = SaveJson["server"]["PlayerRankExp"];
+    document.getElementById("money_holder").value = SaveJson["server"]["Money"];
+    document.getElementById("snail_holder").value = SaveJson["server"]["Shell"];
+    var playerInfo = SaveJson["client"]["Common"]["Coordinates"];
+    click_clickable_sett(getElementByRsdbId("player_playertype", playerInfo["ModelType"]));
+    onChangePlayerType(playerInfo["ModelType"]);
+    click_clickable_sett(getElementByRsdbId("player_skintone", playerInfo["SkinColor"]));
+    click_clickable_sett(getElementByRsdbId("player_eyecolor", playerInfo["EyeColor"]));
+    click_clickable_sett(getElementByRsdbId("player_hair", playerInfo["HairId"]));
+    click_clickable_sett(getElementByRsdbId("player_eyebrow", playerInfo["Eyebrow"]));
+    click_clickable_sett(getElementByRsdbId("player_bottom", playerInfo["BottomId"]));
 }
 
 async function onDecryptSave(){
@@ -38,18 +60,90 @@ async function onDecryptSave(){
     loadSave();
 }
 
+async function onEditSave(){
+    let formData = new FormData();
+    formData.append('save', SaveRaw);
+    formData.append('edits', JSON.stringify(SaveEdits, null, 2));
+
+    const response = await fetch('https://flexlion3.herokuapp.com/save/edit', {
+		method: "POST", 
+		body: formData
+	});
+
+    body = await response.blob();
+
+    const errorItem = document.getElementById("edit_error");
+    try {
+        errorJson = JSON.parse(await body.text());
+        errorItem.textContent = errorJson["error"];
+    } catch(error){
+        downloadFile(body, "save.dat", "text/plain");
+        errorItem.textContent = "";
+    }
+}
+
 async function load_options(){
+    loadSkinTones();
+    loadEyeColors();
+    loadHairs();
+    loadEyebrows();
+    loadPants();
+
 	$('.player_name_holder').on("propertychange change click keyup input paste", function(event){
-        SaveEdits["player_name"] = event.target.value;
+        var name_holder = event.target;
+        if(name_holder.value.length > 16) name_holder.value = name_holder.value.slice(0, 16);
+        SaveEdits["player_name"] = name_holder.value;
     });
 	$('.player_identifier_holder').on("propertychange change click keyup input paste", function(event){
-        SaveEdits["player_identifier"] = event.target.value;
+        var id_holder = event.target;
+        if(id_holder.value.length > 4) id_holder.value = id_holder.value.slice(0, 4);
+        SaveEdits["player_identifier"] = id_holder.value;
     });
 	$('.player_rank_holder').on("propertychange change click keyup input paste", function(event){
-        SaveEdits["player_rank"] = event.target.value;
+        var rank_holder = event.target;
+        if(rank_holder.value > 99) rank_holder.value = 99;
+        if(rank_holder.value < 1) rank_holder.value = 1;
+        SaveEdits["player_rank"] = rank_holder.value - 1;
     });
 	$('.player_rank_exp_holder').on("propertychange change click keyup input paste", function(event){
         SaveEdits["player_rank_exp"] = event.target.value;
+    });
+    $('.money_holder').on("propertychange change click keyup input paste", function(event){
+        var money_holder = event.target;
+        if(money_holder.value > 9999999) money_holder.value = 9999999;
+        if(money_holder.value < 0) money_holder.value = 0;
+        SaveEdits["money"] = money_holder.value;
+    });
+    $('.snail_holder').on("propertychange change click keyup input paste", function(event){
+        var snail_holder = event.target;
+        if(snail_holder.value > 999) snail_holder.value = 999;
+        if(snail_holder.value < 0) snail_holder.value = 0;
+        SaveEdits["snails"] = snail_holder.value;
+    });
+    $('.player_playertype').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_playertype"] = event.target.getAttribute("rsdb_id");
+        onChangePlayerType(event.target.getAttribute("rsdb_id"));
+    });
+    $('.player_skintone').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_skintone"] = event.target.getAttribute("rsdb_id");
+    });
+    $('.player_eyecolor').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_eyecolor"] = event.target.getAttribute("rsdb_id");
+    });
+    $('.player_hair').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_hair"] = event.target.getAttribute("rsdb_id");
+    });
+    $('.player_eyebrow').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_eyebrow"] = event.target.getAttribute("rsdb_id");
+    });
+    $('.player_bottom').click( function(event){
+        click_clickable_sett(event.target);
+        SaveEdits["player_bottom"] = event.target.getAttribute("rsdb_id");
     });
 }
 
@@ -62,7 +156,8 @@ $(document).ready(async () => {
         (async () => { GearInfoShoes = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/GearInfoShoes.json") })(),
         (async () => { HairInfo = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/HairInfo.json") })(),
         (async () => { EyebrowInfo = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/EyebrowInfo.json") })(),
-        (async () => { BottomInfo = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/BottomInfo.json") })(),
+        (async () => { BottomInfo = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/BottomInfo.json") })()//,
+        //(async () => { NameplateInfo = await fetchJson("https://raw.githubusercontent.com/Flexlion/flexlion.github.io/master/assets/RSDB/NamePlateBgInfo.json") })(),
     ]);
     await load_options();
 });
